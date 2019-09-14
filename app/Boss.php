df3d942f10e52c929e0fede7cb64267fc26f9dbe
Eryens <david.saigne@hotmail.fr>
2019-09-14 14:53:57 +0200

    boss raids entity relationships

--- app/Boss.php
+++ app/Boss.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Boss extends Model
 {
-    //
+    public function bosses()
+    {
+        return $this->hasMany('App\Boss');
+    }
 }
--- app/Raids.php
+++ app/Raids.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Raids extends Model
 {
-    //
+    public function raid()
+    {
+        return $this->belingsTo('App\Raids');
+    }
 }
