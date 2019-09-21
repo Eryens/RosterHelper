20c7bfe8438fde05e62143ea4fc22dc10c12af42
Eryens <david.saigne@hotmail.fr>
2019-09-21 18:13:22 +0200

    player class added

---
+++ resources/js/components/UserList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h4>Users</h4>
+            <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Class</th>
+                <th></th>
+            </tr>
+        </thead>
+    </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: [],
+            form:( {
+                id: '',
+                confirmed: '',
+            }),
+        }
+    },
+}
+</script>
